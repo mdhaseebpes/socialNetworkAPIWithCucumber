fc17a0430329a2be66828977340dcbeffe092437
Haseeb0209 <mdhaseebpes@gmail.com>
2023-08-15 14:56:33 +0530

    Update Constants.java

--- src/main/java/com/social/utils/Constants.java
+++ src/main/java/com/social/utils/Constants.java
@@ -3,7 +3,12 @@ package com.social.utils;
 public interface Constants {
 
 
-    String BASE_URI = "https://jsonplaceholder.typicode.com";
+     String BASE_URI = "https://"+System.getProperty("env")+".typicode.com";
+
+    String QA_BASE_URI = "https://jsonplaceholder.typicode.com";
+
+    String STAGE_BASE_URI = "https://jsonplaceholder.typicode.com";
+    
     String COMMENTS_ENDPOINT = "/comments";
 
     String POST_ENDPOINT = "/posts";
