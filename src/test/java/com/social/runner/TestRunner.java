f64334227752f11360b07539234736c9e785e030
Haseeb0209 <mdhaseebpes@gmail.com>
2023-08-29 12:58:07 +0530

    Update TestRunner.java
    Adding execution to be executed in parallel using thread count

--- src/test/java/com/social/runner/TestRunner.java
+++ src/test/java/com/social/runner/TestRunner.java
@@ -1,9 +1,10 @@
 package com.social.runner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import static io.cucumber.testng.CucumberOptions.SnippetType.CAMELCASE;
 
@@ -15,20 +16,42 @@ import static io.cucumber.testng.CucumberOptions.SnippetType.CAMELCASE;
                 "com.social.listener.CucumberExtentReportListener:",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json",
-        "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"},
+                "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"},
         snippets = CAMELCASE,
         monochrome=true
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-    @BeforeClass
-    public void beforeClass(){
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
     }
 
 
-    @AfterClass
-    public void afterClass(){
+    @Test(groups = "cucumber", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
+    public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+        // the 'featureWrapper' parameter solely exists to display the feature
+        // file in a test report
+        testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
     }
 
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        if (testNGCucumberRunner == null) {
+            return new Object[0][0];
+        }
+        return testNGCucumberRunner.provideScenarios();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() {
+        if (testNGCucumberRunner == null) {
+            return;
+        }
+        testNGCucumberRunner.finish();
+    }
 }
+
